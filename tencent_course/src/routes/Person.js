f5162f580e3dc812f750e239697c53fa0d77491f
1041489964 <1041489964@qq.com>
2018-07-03 19:01:26 +0800

    info页面完成

--- tencent_course/src/routes/Person.js
+++ tencent_course/src/routes/Person.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Switch,Route,Redirect} from 'react-router-dom';
+
+/*IMPORT COMPONENT*/
+import Login from './person/Login';
+import Register from './person/Register';
+import Info from './person/Info';
+import Tip from './person/Tip';
 
 class Person extends React.Component {
     constructor(props, context) {
@@ -8,7 +15,12 @@ class Person extends React.Component {
 
     render() {
         return <section>
-            Person
+            <Switch>
+                <Route path='/person/info' component={Login}/>
+                <Route path='/person/login' component={Login}/>
+                <Route path='/person/register' component={Register}/>
+                <Redirect from='/person' to='/person/info'/>
+            </Switch>
         </section>
     }
 }
