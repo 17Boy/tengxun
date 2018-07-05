84903d1887ad5ffd1b1c928ace5b5916cd7c44be
593562612@qq.com <593562612@qq.com>
2018-07-05 11:06:48 +0800

    数据绑定2

--- tencent_course/src/component/Search.js
+++ tencent_course/src/component/Search.js
@@ -10,7 +10,6 @@ class Search extends React.Component {
     }
 
     render() {
-        console.log(this.props)
         return <section className={'searchBox'}>
             <Link to={'/course'}>
                 <i className={'logo'} />
